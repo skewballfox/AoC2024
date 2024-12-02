c7b20725bd0f3276bec234847d8e924bf5f6dd48
Joshua Ferguson <joshua.ferguson.273@gmail.com>
2024-12-01 18:35:36 -0600

    inlined parsing function for part2, switched to counting entries in right list

--- src/day1.rs
+++ src/day1.rs
@@ -14,25 +14,22 @@ pub fn get_number(input: &str) -> (u32, &str) {
         input[input.len()..].trim_start(),
     )
 }
-
+#[inline(always)]
 pub fn input_generator(input: &str) -> (Vec<u32>, Vec<u32>) {
     let mut v = Vec::with_capacity(1000);
     let mut v2 = Vec::with_capacity(1000);
     //let input = input.trim_start();
     let mut inp = input;
-    let mut num;
-    loop {
-        if inp.is_empty() {
-            return (v, v2);
-        } else {
-            (num, inp) = get_number(inp);
-            v.push(num);
+    let mut num = 0;
+    (0..1000).for_each(|_| {
+        (num, inp) = get_number(inp);
+        v.push(num);
 
-            (num, inp) = get_number(inp);
+        (num, inp) = get_number(inp);
 
-            v2.push(num);
-        }
-    }
+        v2.push(num);
+    });
+    (v, v2)
 }
 
 #[aoc(day1, part1)]
@@ -57,62 +54,26 @@ pub fn part1(input: &str) -> u32 {
     })
 }
 
-#[inline(always)]
-pub fn count_values(arr: &[u32], idx: usize) -> u32 {
-    let value = arr[idx];
-
-    let mut start = idx;
-    while start > 0 && arr[start - 1] == value {
-        start -= 1;
-    }
-
-    let mut stop = idx;
-    while stop < arr.len() - 1 && arr[stop + 1] == value {
-        stop += 1;
-    }
-
-    (stop - start) as u32 + 1
-}
-
-pub fn bin_search_count(arr: &[u32], value: u32) -> u32 {
-    let (mut left, mut right) = (0, arr.len() - 1);
-    if arr[left] == value {
-        return count_values(arr, left);
-    }
-    if arr[right] == value {
-        return count_values(arr, right);
-    }
-    let mut mid;
-    while left <= right {
-        mid = left + (right - left) / 2;
-
-        match value.cmp(&arr[mid]) {
-            std::cmp::Ordering::Less => {
-                right = if mid > 0 {
-                    mid - 1
-                } else {
-                    return 0;
-                }
-            }
-            std::cmp::Ordering::Greater => left = mid + 1,
-            std::cmp::Ordering::Equal => return count_values(arr, mid),
-        }
-    }
-    0
-}
-
 #[aoc(day1, part2)]
 pub fn part2(input: &str) -> u32 {
-    let (left, mut right) = input_generator(input);
-    right.sort();
-    let mut prev_scores = HashMap::with_capacity(1000);
-    let res = left.iter().fold(0, |similarity_score, l| {
-        similarity_score
-            + *{
-                prev_scores
-                    .entry(l)
-                    .or_insert_with(|| l * bin_search_count(&right, *l))
-            }
+    let mut left = Vec::with_capacity(1000);
+    let mut right_counter = HashMap::with_capacity(1000);
+    //let input = input.trim_start();
+    let mut inp = input;
+    let mut num = 0;
+    (0..1000).for_each(|_i: i32| {
+        (num, inp) = get_number(inp);
+        left.push(num);
+
+        (num, inp) = get_number(inp);
+
+        right_counter
+            .entry(num)
+            .and_modify(|v| *v += 1)
+            .or_insert(1);
     });
-    res
+
+    left.iter().fold(0, |similarity_score, l| {
+        similarity_score + { l * right_counter.get(l).unwrap_or_else(|| &0) }
+    })
 }
