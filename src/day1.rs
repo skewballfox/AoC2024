bb0eb8030eefc60e32e6d85112733ef3f6aa8c72
Joshua Ferguson <joshua.ferguson.273@gmail.com>
2024-12-02 10:33:05 -0600

    switched to old-new state for day1 part1, and used crate for aradix sort

--- src/day1.rs
+++ src/day1.rs
@@ -1,6 +1,6 @@
-use std::collections::{HashMap, HashSet};
-
 use aoc_runner_derive::aoc;
+use std::collections::HashMap;
+use voracious_radix_sort::RadixSort;
 
 #[inline(always)]
 pub fn get_number(input: &str) -> (u32, &str) {
@@ -35,23 +35,27 @@ pub fn input_generator(input: &str) -> (Vec<u32>, Vec<u32>) {
 #[aoc(day1, part1)]
 pub fn part1(input: &str) -> u32 {
     let (mut left, mut right) = input_generator(input);
-    left.sort();
-    right.sort();
-    let mut pairs = HashSet::with_capacity(1000);
-    left.iter().zip(right.iter()).fold(0, |sum, (l, r)| {
-        sum + {
-            if pairs.contains(&(l, r)) || l == r {
-                0
+    left.voracious_sort();
+    right.voracious_sort();
+
+    let mut old_pair = (left[999], right[999]);
+    let mut comp = |p: (u32, u32)| -> u32 {
+        if p == old_pair {
+            0
+        } else {
+            old_pair = p;
+            if p.0 > p.1 {
+                p.0 - p.1
             } else {
-                pairs.insert((l, r));
-                if l > r {
-                    l - r
-                } else {
-                    r - l
-                }
+                p.1 - p.0
             }
         }
-    })
+    };
+    let sum = comp((left[0], right[0]));
+    left.iter()
+        .skip(1)
+        .zip(right.iter().skip(1))
+        .fold(sum, |sum, (l, r)| sum + comp((*l, *r)))
 }
 
 #[aoc(day1, part2)]
