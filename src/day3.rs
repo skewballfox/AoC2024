6d9abe31c3de1174bd42e881bce98fbad0117bdf
Joshua Ferguson <joshua.ferguson.273@gmail.com>
2024-12-04 10:33:21 -0600

    inlined parsing for d3 solutions

--- src/day3.rs
+++ src/day3.rs
@@ -4,7 +4,6 @@ use aoc_runner_derive::aoc;
 //     _mm256_set1_epi8,
 // };
 use std::ops::Range;
-
 const MUL_BYTES: [u8; 3] = [b'u', b'l', b'('];
 const DONT_BYTES: [u8; 6] = [b'o', b'n', b'\'', b't', b'(', b')'];
 const DO_BYTES: [u8; 3] = [b'o', b'(', b')'];
@@ -183,10 +182,72 @@ fn get_muls_toggle(input: &[u8]) -> Vec<u32> {
 #[aoc(day3, part1)]
 pub fn part1(input: &str) -> u32 {
     let input = input.as_bytes();
-    let muls = get_muls(input);
+
     let mut sum = 0;
-    for prod in muls {
-        sum += prod;
+    let mut start = 0;
+    let mut state = ParserState::Seeking;
+    let mut first = 0;
+    let mut second = 0;
+    'outer: while start < input.len() {
+        match state {
+            ParserState::Seeking => {
+                if input[start] == b'm' {
+                    if start + 7 < input.len() && &input[start + 1..start + 4] == &MUL_BYTES {
+                        state = ParserState::FirstInput;
+                        start += 4;
+                    } else {
+                        start += 1;
+                    }
+                } else {
+                    start += 1;
+                }
+            }
+            ParserState::FirstInput => {
+                let mut offset = start;
+
+                while offset < input.len() {
+                    match input[offset] {
+                        b',' => {
+                            first = get_digit(&input[start..offset]);
+                            start = offset + 1;
+                            state = ParserState::SecondInput;
+                            continue 'outer;
+                        }
+                        c if ASCII_DIGITS.contains(&c) => {
+                            offset += 1;
+                        }
+                        _ => {
+                            start = offset;
+                            state = ParserState::Seeking;
+                            continue 'outer;
+                        }
+                    }
+                }
+            }
+            ParserState::SecondInput => {
+                let mut offset = start;
+
+                while offset < input.len() {
+                    match input[offset] {
+                        b')' => {
+                            second = get_digit(&input[start..offset]);
+                            sum += first * second;
+                            start = offset + 1;
+                            state = ParserState::Seeking;
+                            continue 'outer;
+                        }
+                        c if ASCII_DIGITS.contains(&c) => {
+                            offset += 1;
+                        }
+                        _ => {
+                            start = offset;
+                            state = ParserState::Seeking;
+                            continue 'outer;
+                        }
+                    }
+                }
+            }
+        }
     }
     sum
 }
@@ -194,11 +255,91 @@ pub fn part1(input: &str) -> u32 {
 #[aoc(day3, part2)]
 pub fn part2(input: &str) -> u32 {
     let input = input.as_bytes();
-    let muls = get_muls_toggle(input);
+
     let mut sum = 0;
-    for prod in muls {
-        sum += prod;
+    let mut start = 0;
+    let mut state = TogglableParserState::Seeking;
+    let mut first = 0;
+    let mut second = 0;
+    'outer: while start < input.len() {
+        match state {
+            TogglableParserState::Seeking => {
+                if input[start] == b'm' {
+                    if start + 7 < input.len() && &input[start + 1..start + 4] == &MUL_BYTES {
+                        state = TogglableParserState::FirstInput;
+                        start += 4;
+                    } else {
+                        start += 1;
+                    }
+                } else if input[start] == b'd' {
+                    if start + 6 < input.len() && &input[start + 1..start + 7] == &DONT_BYTES {
+                        state = TogglableParserState::Disabled;
+                        start += 6;
+                    } else {
+                        start += 1;
+                    }
+                } else {
+                    start += 1;
+                }
+            }
+            TogglableParserState::FirstInput => {
+                let mut offset = start;
+
+                while offset < input.len() {
+                    match input[offset] {
+                        b',' => {
+                            first = get_digit(&input[start..offset]);
+                            start = offset + 1;
+                            state = TogglableParserState::SecondInput;
+                            continue 'outer;
+                        }
+                        c if ASCII_DIGITS.contains(&c) => {
+                            offset += 1;
+                        }
+                        _ => {
+                            start = offset;
+                            state = TogglableParserState::Seeking;
+                            continue 'outer;
+                        }
+                    }
+                }
+            }
+            TogglableParserState::SecondInput => {
+                let mut offset = start;
+
+                while offset < input.len() {
+                    match input[offset] {
+                        b')' => {
+                            second = get_digit(&input[start..offset]);
+                            sum = first * second;
+                            start = offset + 1;
+                            state = TogglableParserState::Seeking;
+                            continue 'outer;
+                        }
+                        c if ASCII_DIGITS.contains(&c) => {
+                            offset += 1;
+                        }
+                        _ => {
+                            start = offset;
+                            state = TogglableParserState::Seeking;
+                            continue 'outer;
+                        }
+                    }
+                }
+            }
+            TogglableParserState::Disabled => {
+                if input[start] == b'd' {
+                    if start + 3 < input.len() && &input[start + 1..start + 4] == &DO_BYTES {
+                        state = TogglableParserState::Seeking;
+                        start += 3;
+                        continue 'outer;
+                    }
+                }
+                start += 1;
+            }
+        }
     }
+
     sum
 }
 
